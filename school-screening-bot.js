83425469d1220c647c93b8e55c11a56b8fe31310
pavitar <41386653+pavitarsaini@users.noreply.github.com>
2021-02-04 00:24:07 -0500

    init tests

--- school-screening-bot.js
+++ school-screening-bot.js
@@ -1,1 +1,88 @@
 document.querySelector('.button__StyledButton-sc-1hmy6jw-0').click();
+
+document.getElementById("student").click();
+
+const links = Array.from(document.querySelectorAll('.button__StyledButton-sc-1hmy6jw-0'));
+
+links.forEach((link) => {
+  if (link.textContent === 'No') {
+    link.click();
+  }
+})
+
+document.getElementById("none_of_the_above").click();
+
+document.querySelector('.button__StyledButton-sc-1hmy6jw-0').click();
+
+setTimeout(function(){ document.querySelector('.button__StyledButton-sc-1hmy6jw-0').click();}, 500);
+
+
+
+setTimeout(function(){ document.getElementById("student").click();
+ }, 5000);
+
+
+setTimeout(function(){ 
+document.querySelector('.button__StyledButton-sc-1hmy6jw-0').click(); }, 5000);
+
+
+
+setTimeout(function(){ document.querySelector("#reach-skip-nav > div > div > div > div.ontario-form__button-wrapper.ontario-text-center > button:nth-child(1)").click(); }, 5000);
+
+
+setTimeout(function(){ document.getElementById("none_of_the_above").click();
+document.querySelector('.button__StyledButton-sc-1hmy6jw-0').click(); }, 5000);
+
+
+b()
+b()
+
+
+
+var a;
+for(a = 0; a < 5; a++){
+    setTimeout(function(){ document.querySelector("#reach-skip-nav > div > div > div > div.ontario-form__button-wrapper.ontario-text-center > button:nth-child(1)").click(); }, 100);
+}
+
+
+document.querySelector('.button__StyledButton-sc-1hmy6jw-0').click();
+
+
+    if (!document.getElementById("student").size()) {
+      window.requestAnimationFrame(test);
+    }else {
+        document.getElementById("student").click();
+        document.querySelector('.button__StyledButton-sc-1hmy6jw-0').click();
+     }
+
+// *************************
+
+function a (){
+     if (document.getElementsByTagName("h1")[0].innerHTML.indexOf('COVID-19 school and child care screening') > -1){
+        document.querySelector('.button__StyledButton-sc-1hmy6jw-0').click();
+    }else(a())
+
+}
+
+
+
+    function b (){
+        setTimeout(function(){
+
+        if (document.getElementsByTagName("h2")[0].innerHTML.indexOf('COVID-19 school and child care screening') > -1){
+             document.getElementById("student").click();
+document.querySelector('.button__StyledButton-sc-1hmy6jw-0').click(); 
+       }else(b())
+
+    }, 2000);
+   
+   }
+
+
+   a()
+   b()
+
+
+
+
+  
